d758fc14b4bf9d257933422eec205224ed1ba12e
mantagen <mathsgregory@gmail.com>
2015-09-19 23:50:36 +0100

    sending test js file

---
+++ temp.js
@@ -0,0 +1,27 @@
+function toggleClass(elem, className) {
+    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace( ' ' + className + ' ' , ' ' );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+function arrFromClass(elem, className) {
+  return Array.prototype.slice.call(elem.getElementsByClassName(className));
+}
+
+var postTitles = arrFromClass(document, 'minimalpost');
+
+postTitles.forEach(function(elem) {
+  elem.getElementsByClassName('blogtitlebanner')[0].addEventListener('click', function(event) {
+    toggleClass(elem.getElementsByClassName('blog-content')[0], 'bloginvis')
+  });
+});
